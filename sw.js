122a93ee28b92124778f7375a9d9d3bb2fcae09b
dreambeyondlabs <dreambeyondlimitx@gmail.com>
2026-06-18 10:48:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'floor-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      // cache same-origin and font files as they're fetched
+      const url = new URL(req.url);
+      if (url.origin === location.origin || url.host.includes('gstatic') || url.host.includes('googleapis')) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
